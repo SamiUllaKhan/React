c66d06f2aec73447b9fa48b96b304509440e2c3c
Sami <sami.khan@plexussquare.com>
2021-07-20 15:00:19 +0530

    - Responsive issuedb.Json added

--- pages/register.js
+++ pages/register.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/no-unescaped-entities */
 import {
   Stack,
   Flex,
@@ -8,12 +10,12 @@ import {
   FormControl,
   FormLabel,
   Select,
-  FormErrorMessage,
-  FormHelperText,
   Input,
   Textarea,
   useColorMode,
-  Button
+  Button,
+  Grid,
+  SimpleGrid,
 } from '@chakra-ui/react'
 import DarkModeSwitch from '../components/DarkModeSwitch'
 
@@ -30,82 +32,85 @@ export default function register() {
           )}
           <DarkModeSwitch />
         </Flex>
-        <Flex
-          flexDirection="column"
-          p={8}
-          borderWidth="1px"
-          borderRadius="lg"
-          width="400px"
-        >
-          <Heading as="h5" size="sm" mb={10}>
-            <Center mb={2}>Register Now!</Center>
-            <Center>
-              <Text fontSize="xs">
-                We'll never share your information to any
-              </Text>
-            </Center>
-          </Heading>
+        <Grid
+            gap={2}>
+            <Heading as="h5" size="sm" colSpan={1}>
+                  <Center mb={2}>Register Now!</Center>
+                  <Center>
+                    <Text fontSize="xs">
+                      We'll never share your information to any
+                    </Text>
+                  </Center>
+              </Heading>
+          </Grid>
+        <Flex p={8} borderWidth="1px" borderRadius="lg">
+            <SimpleGrid
+              columns={{ sm: 1, md: 2 }}
+              flexDirection="column"
+              spacingX="20px"
+              spacingY="10px">
+            <FormControl id="first-name" isRequired>
+              <FormLabel>First name</FormLabel>
+              <Input size="sm" placeholder="First name" />
+            </FormControl>
 
-          <FormControl id="first-name" isRequired>
-            <FormLabel>First name</FormLabel>
-            <Input size="sm" placeholder="First name" />
-          </FormControl>
+            <FormControl id="middle-name">
+              <FormLabel>Middle name</FormLabel>
+              <Input size="sm" placeholder="Middle name" />
+            </FormControl>
 
-          <FormControl id="middle-name">
-            <FormLabel>Middle name</FormLabel>
-            <Input size="sm" placeholder="Middle name" />
-          </FormControl>
+            <FormControl id="Last-name">
+              <FormLabel>Last name</FormLabel>
+              <Input size="sm" placeholder="Last name" />
+            </FormControl>
 
-          <FormControl id="Last-name">
-            <FormLabel>Last name</FormLabel>
-            <Input size="sm" placeholder="Last name" />
-          </FormControl>
+            <FormControl id="country">
+              <FormLabel>Country</FormLabel>
+              <Select size="sm" placeholder="Select country">
+                <option>United Arab Emirates</option>
+                <option>Nigeria</option>
+              </Select>
+            </FormControl>
 
-          <FormControl id="country">
-            <FormLabel>Country</FormLabel>
-            <Select size="sm" placeholder="Select country">
-              <option>United Arab Emirates</option>
-              <option>Nigeria</option>
-            </Select>
-          </FormControl>
+            <FormControl id="state">
+              <FormLabel>State</FormLabel>
+              <Input size="sm" placeholder="State" />
+            </FormControl>
 
-          <FormControl id="state">
-            <FormLabel>State</FormLabel>
-            <Input size="sm" placeholder="State" />
-          </FormControl>
+            <FormControl id="city">
+              <FormLabel>City</FormLabel>
+              <Input size="sm" placeholder="City" />
+            </FormControl>
 
-          <FormControl id="city">
-            <FormLabel>City</FormLabel>
-            <Input size="sm" placeholder="City" />
-          </FormControl>
+            <FormControl id="pincode">
+              <FormLabel>Pincode / ZipCode</FormLabel>
+              <Input size="sm" type="number" placeholder="Pincode / ZipCode" />
+            </FormControl>
 
-          <FormControl id="pincode">
-            <FormLabel>Pincode / ZipCode</FormLabel>
-            <Input size="sm" type="number" placeholder="Pincode / ZipCode" />
-          </FormControl>
+            <FormControl id="address" isRequired>
+              <FormLabel>Address</FormLabel>
+              <Textarea placeholder="Address" />
+            </FormControl>
 
-          <FormControl id="address" isRequired>
-            <FormLabel>Address</FormLabel>
-            <Textarea placeholder="Address" />
-          </FormControl>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input size="sm" type="email" placeholder="Email address" />
+            </FormControl>
 
-          <FormControl id="email" isRequired>
-            <FormLabel>Email address</FormLabel>
-            <Input size="sm" type="email" placeholder="Email address" />
-          </FormControl>
+            <FormControl id="mobile" isRequired>
+              <FormLabel>Mobile</FormLabel>
+              <Input size="sm" type="number" placeholder="Mobile" />
+            </FormControl>
 
-          <FormControl id="mobile" isRequired>
-            <FormLabel>Mobile</FormLabel>
-            <Input size="sm" type="number" placeholder="Mobile" />
-          </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input size="sm" type="password" placeholder="Password" />
+            </FormControl>
 
-          <FormControl id="password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input size="sm" type="password" placeholder="Password" />
-          </FormControl>
-
-          <Button mt={5} colorScheme="teal" size="sm">Register</Button>
-
+            <Button mt={5} colorScheme="teal" size="sm">
+              Register
+            </Button>
+          </SimpleGrid>
         </Flex>
       </Stack>
     </>
